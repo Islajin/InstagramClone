4db65b7974feb14fedd601ab3877b9db09f04c0b
Isla <Islajiin@gmail.com>
2025-05-03 08:32:03 +0900

    [file]- LoginView 추가

---
+++ InstagramClone/Login/LoginView.swift
@@ -0,0 +1,12 @@
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        
+        LinearGradient(colors: [Color.blue], startPoint: <#T##UnitPoint#>, endPoint: <#T##UnitPoint#>)
+    }
+}
+
+#Preview {
+    LoginView()
+}
