755c8e8124b44ab40b042991cae80df43d52ea9d
Isla <Islajiin@gmail.com>
2025-05-03 01:41:09 +0900

    [feat]- uploadPost() 와 uploadImage() 구현
    uploadPost() 는 게시글 업로드 함수
    uploadImage()는 사진 업로드 함수

--- InstagramClone/NewPost/NewPostViewModel.swift
+++ InstagramClone/NewPost/NewPostViewModel.swift
@@ -1,12 +1,13 @@
 import SwiftUI
 import PhotosUI
+import FirebaseStorage
 
 @Observable
 class NewPostViewModel {
     var caption = ""
     var selectedItem : PhotosPickerItem?
     var postImage: Image?
-    
+    var uiImage : UIImage?
     func convertImage(item: PhotosPickerItem?) async {
         
         //item 을 안전하게 꺼냄 -> data를 컴퓨터가 읽을 수 있는 값으로 변경-> UIImage(UIKit에서 사용하는 이미지 형식)으로 변경->Image(SwiftUI에서 사용하는 이미지 형식)으로 변경
@@ -15,7 +16,49 @@ class NewPostViewModel {
         guard let data = try? await item.loadTransferable(type: Data.self) else {return}
         guard let uiImage = UIImage(data: data) else {return}
         self.postImage = Image(uiImage: uiImage)
+        self.uiImage = uiImage
         
         
     }
+    
+    
+
+    //게시글 업로드 함수 (게시글 : 글 + 사진)
+    func uploadPost() async {
+        guard let uiImage else {return}
+        let url = await uploadImage(uiImage: uiImage)
+    }
+    
+    
+    //사진 업로드 함수
+    //사진 업로드 시에 UI 이미지가 필요하고, 우리가 사진을 올린 주소를 리턴 값으로 받을 것임(옵셔널 String? )
+    //여기도 Task로 감싸지 않고, 상위로 넘김
+    func uploadImage(uiImage : UIImage) async -> String? {
+        
+        //uiImage를 받아서 jpeg파일을 0.5의 퀄리티로 압축
+        guard let imageData = uiImage.jpegData(compressionQuality: 0.5) else { return nil }
+        //fileName = 파일 이름을 UUID().uuidString - 파일의 랜덤한 문구를 정해줌
+        let fileName =  UUID().uuidString
+        
+        //이걸 스토리지에 저장하려면 .reference 로 사진이 저장될 위치를 설정- 이미지라는 폴더 내에 파일 이름으로 스토리지에 저장해 달라는 것임, 해당 위치를 reference 변수에 담음
+        let reference = Storage.storage().reference (withPath: "/images/\(fileName)")
+        
+        
+        do{ //putDataAsync 와 downloadURL 모두 Async 로 Task 가 필요한데, 그냥 상위로 넘김
+            //reference에 우리가 압축한 이미지 데이터를 넣어줌, metaData는 이미지 정보를 가짐
+            let metaData = try await reference.putDataAsync(imageData)
+            
+            //이미지가 올라간 거를 게시글에 저장할껀데, 데이터는 이 이미지 스토리지에 저장하고, 이미지가 올라간 url만 이 게시글에 저장해 놓도록 할꺼임
+            let url = try await reference.downloadURL()
+            
+            return url.absoluteString
+        }
+        catch {
+            print("\(error.localizedDescription)")
+            return nil
+        }
+        
+    }
+    
+    
 }
