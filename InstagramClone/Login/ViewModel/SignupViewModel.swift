e25fd60364e68e2fd89901a2e27dd5dcb8a9f874
Isla <Islajiin@gmail.com>
2025-05-03 18:48:52 +0900

    [feat]- 회원가입 뷰 모델 구현

---
+++ InstagramClone/Login/ViewModel/SignupViewModel.swift
@@ -0,0 +1,23 @@
+//
+//  SignupViewModel.swift
+//  InstagramClone
+//
+//  Created by yeonjin on 5/3/25.
+//
+
+import Foundation
+
+
+@Observable
+class SignupViewModel {
+    
+    var email = ""
+    var password = ""
+    var name = ""
+    var username = ""
+    
+    func createUser() {
+        
+        
+    }
+}
