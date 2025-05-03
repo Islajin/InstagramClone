237b5fdae91de8862f8627472f41ab201ed8c7b9
Isla <Islajiin@gmail.com>
2025-05-03 09:30:52 +0900

    [feat]- ZStack을 NavigationStack 으로 Embed

--- InstagramClone/Login/LoginView.swift
+++ InstagramClone/Login/LoginView.swift
@@ -4,68 +4,72 @@ struct LoginView: View {
     
     var body: some View {
         
-        ZStack {
-            GradientBackgroundView()
-            VStack{
-                Spacer()
-                
-                Image("instagramLogo")
-                    .resizable()
-                    .frame(width:57, height: 57)
-                    .scaledToFit()
-                
-                Spacer()
-                
-                VStack {
-                    TextField("이메일 주소", text: .constant(""))
-                        .textInputAutocapitalization(.never) //대문자 방지
-                        .padding(12)
-                        .background(.white)
-                        .clipShape(RoundedRectangle(cornerRadius: 10))
-                        .overlay{
-                            RoundedRectangle(cornerRadius: 10)
-                                .stroke(.gray, lineWidth: 1.0)
-                        }
-                        .padding(.horizontal)
+        NavigationStack {
+            ZStack {
+                GradientBackgroundView()
+                VStack{
+                    Spacer()
                     
+                    Image("instagramLogo")
+                        .resizable()
+                        .frame(width:57, height: 57)
+                        .scaledToFit()
                     
-                    SecureField("비밀번호", text: .constant(""))
-                        .textInputAutocapitalization(.never) //대문자 방지
-                        .padding(12)
-                        .background(.white)
-                        .clipShape(RoundedRectangle(cornerRadius: 10))
-                        .overlay{
-                            RoundedRectangle(cornerRadius: 10)
-                                .stroke(.gray, lineWidth: 1.0)
-                        }
-                        .padding(.horizontal)
+                    Spacer()
                     
-                    Button{
-                    } label:{
-                        Text("로그인")
-                            .frame(width: 363, height: 42)
-                            .foregroundStyle(.white)
-                            .background(.blue)
-                            .clipShape(RoundedRectangle(cornerRadius: 20))
+                    VStack {
+                        TextField("이메일 주소", text: .constant(""))
+                            .textInputAutocapitalization(.never) //대문자 방지
+                            .padding(12)
+                            .background(.white)
+                            .clipShape(RoundedRectangle(cornerRadius: 10))
+                            .overlay{
+                                RoundedRectangle(cornerRadius: 10)
+                                    .stroke(.gray, lineWidth: 1.0)
+                            }
+                            .padding(.horizontal)
                         
-                    }.padding()
+                        
+                        SecureField("비밀번호", text: .constant(""))
+                            .textInputAutocapitalization(.never) //대문자 방지
+                            .padding(12)
+                            .background(.white)
+                            .clipShape(RoundedRectangle(cornerRadius: 10))
+                            .overlay{
+                                RoundedRectangle(cornerRadius: 10)
+                                    .stroke(.gray, lineWidth: 1.0)
+                            }
+                            .padding(.horizontal)
+                        
+                        Button{
+                        } label:{
+                            Text("로그인")
+                                .frame(width: 363, height: 42)
+                                .foregroundStyle(.white)
+                                .background(.blue)
+                                .clipShape(RoundedRectangle(cornerRadius: 20))
+                            
+                        }.padding()
+                        
+                        Text("비밀번호를 잊으셨나요?")
+                    }
                     
-                    Text("비밀번호를 잊으셨나요?")
+                    Spacer()
+                    NavigationLink{
+                        EnterEmailView()
+                    }label:{
+                        Text("새 계정 만들기")
+                            .fontWeight(.bold)
+                            .frame(width: 363, height: 42)
+                            .overlay{
+                                RoundedRectangle(cornerRadius : 20)
+                                    .stroke(.blue, lineWidth: 1.0)
+                            }
+                            .foregroundStyle(.blue)
+                            .clipShape(RoundedRectangle(cornerRadius: 20))
+                    }
+                    
                 }
-                
-                Spacer()
-                Button{}label:{
-                    Text("새 계정 만들기")
-                        .fontWeight(.bold)
-                        .frame(width: 363, height: 42)
-                        .overlay{
-                            RoundedRectangle(cornerRadius : 20)
-                                .stroke(.blue, lineWidth: 1.0)
-                        }
-                        .foregroundStyle(.blue)
-                        .clipShape(RoundedRectangle(cornerRadius: 20))
-                }
-                
             }
         }
         
