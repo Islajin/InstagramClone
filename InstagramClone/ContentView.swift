b437dee042f0773ec93be5bb874b702016bae594
Isla <Islajiin@gmail.com>
2025-05-04 06:34:14 +0900

    [feat]- 로그인 분기 처리를 AuthManager 로 처리

--- InstagramClone/ContentView.swift
+++ InstagramClone/ContentView.swift
@@ -6,15 +6,16 @@
 //
 
 import SwiftUI
-
+import FirebaseAuth
 struct ContentView: View {
     
-    
-    @State var isLogin = false
+
     @State var signupViewModel = SignupViewModel()
     
     var body: some View {
-        if isLogin == true {
+        //currentUser 은 State 나 Observable 이런게 세팅이 안되어 있기 때문에 바로바로 SwiftUI가 알 수 없다.
+        //때문에 signupViewModel에서 정의한 UserSession 을 가져오면 뷰에서 감지가 가능하다.
+        if AuthManager.shared.currentUserSession != nil {
             MainTabView()
         }
         else {
