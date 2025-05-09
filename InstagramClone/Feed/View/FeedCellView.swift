8c1d43ad3ee5974f79ae64867f07094d4a41de94
Isla <Islajiin@gmail.com>
2025-05-09 09:33:37 +0900

    [feat]- FeedView에서 Scroll 되는 부분 분리해서 구현

---
+++ InstagramClone/Feed/View/FeedCellView.swift
@@ -0,0 +1,78 @@
+//
+//  FeedCellView.swift
+//  InstagramClone
+//
+//  Created by yeonjin on 5/9/25.
+//
+
+import SwiftUI
+
+struct FeedCellView: View {
+    var body: some View {
+        VStack {
+            Image("image_dragon2")
+                .resizable()
+                .scaledToFit()
+                .frame(maxWidth: .infinity)
+                .overlay (alignment: .top){
+                    HStack {
+                        Image("image_lion4")
+                            .resizable()
+                            .frame(width: 35, height: 35)
+                            .clipShape(Circle())
+                            .overlay {
+                                Circle()
+                                    .stroke(Color(red : 191/255, green: 11/255, blue:100/255), lineWidth: 2)
+                            }
+                        Text("general.cat")
+                            .foregroundStyle(.white)
+                            .bold()
+                        Spacer()
+                        Image(systemName: "line.3.horizontal")
+                            .foregroundStyle(.white)
+                            .imageScale(.large)
+                    }.padding(5)
+                }
+            
+            HStack {
+                Image(systemName: "heart")
+                Image(systemName: "bubble.right")
+                Image(systemName: "paperplane")
+                Spacer()
+                Image(systemName: "bookmark")
+                
+            }.imageScale(.large)
+                .padding(.horizontal)
+            
+            Text("좋아요 25,239개")
+                .font(.footnote)
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .padding(.horizontal)
+            
+            Text("general.cat" + " " + "상세 글 정보")
+                .font(.footnote)
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .padding(.horizontal)
+            
+            Text("댓글 25개 더보기")
+                .foregroundStyle(.gray)
+                .font(.footnote)
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .padding(.horizontal)
+            
+            Text("3일전")
+                .foregroundStyle(.gray)
+                .font(.footnote)
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .padding(.horizontal)
+            
+            
+            
+        }
+        .padding(.bottom)
+    }
+}
+
+#Preview {
+    FeedCellView()
+}
