8bd4f8fe2a3022811d71b6da0bafb5f236996f02
Isla <Islajiin@gmail.com>
2025-05-10 09:56:10 +0900

    [feat]- NavigationStack으로 감싸기

--- InstagramClone/Feed/View/FeedView.swift
+++ InstagramClone/Feed/View/FeedView.swift
@@ -12,45 +12,48 @@ struct FeedView: View {
     @State var viewModel = FeedViewModel()
     
     var body : some View {
-        ScrollView {
-            VStack{
-                HStack {
+        
+        NavigationStack{
+            ScrollView {
+                VStack{
+                    HStack {
+                        
+                        Image("instagramLogo2")
+                            .resizable()
+                            .scaledToFit( )
+                            .frame(width:110)
+                        
+                        Spacer()
+                        
+                        Image(systemName : "heart")
+                            .imageScale(.large)
+                        Image(systemName: "paperplane")
+                            .imageScale(.large)
+                        
+                    }
+                    .padding(.horizontal)
                     
-                    Image("instagramLogo2")
-                        .resizable()
-                        .scaledToFit( )
-                        .frame(width:110)
+                    LazyVStack {
+                        ForEach(viewModel.posts) {post in
+                            let _ = print(post)
+                            //반복문 내에세 프린트를 실행하고 싶으면 이렇게 쓰면 됨
+                            FeedCellView(post: post)
+                            //FeedCellView에 반복으로 모든 post 값 넘겨주기
+                        }
+                    }
                     
                     Spacer()
-                    
-                    Image(systemName : "heart")
-                        .imageScale(.large)
-                    Image(systemName: "paperplane")
-                        .imageScale(.large)
-                    
                 }
-                .padding(.horizontal)
-                
-                LazyVStack {
-                    ForEach(viewModel.posts) {post in
-                        let _ = print(post)
-                        //반복문 내에세 프린트를 실행하고 싶으면 이렇게 쓰면 됨
-                        FeedCellView(post: post)
-                        //FeedCellView에 반복으로 모든 post 값 넘겨주기
-                    }
-                }
-                
-                Spacer()
             }
-        }
-        //스크롤 뷰를 당겨서 업데이트 된 포스트를 보고 싶을 때
-        .refreshable {
-            await viewModel.loadAllPosts( )
-        }
-        
-        //이건 바로바로 업데이트가 됨
-        .task{
-            await viewModel.loadAllPosts( )
+            //스크롤 뷰를 당겨서 업데이트 된 포스트를 보고 싶을 때
+            .refreshable {
+                await viewModel.loadAllPosts( )
+            }
+            
+            //이건 바로바로 업데이트가 됨
+            .task{
+                await viewModel.loadAllPosts( )
+            }
         }
     }
 }
